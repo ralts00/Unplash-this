1d11e7e04f4e1f0750ea8636e3c4122acada40ce
Jonathan Coutiño <ralts00@MacBook-Pro-de-ComproPagoMB.local>
2016-03-04 18:38:52 -0600

    add for

--- unsplashThis.js
+++ unsplashThis.js
@@ -12,13 +12,41 @@
 //funciona
 
 var unsplashThis = document.querySelectorAll(".unsplash-this");
-// var unsplashLink = "https://source.unsplash.com/random"
+var unsplashLink = "https://source.unsplash.com/random"
 
 for(var x = 0; x < unsplashThis.length; x++){
-	unsplashThis[x].style.backgroundImage = "url('https://source.unsplash.com/random')"
+	unsplashThis[x].style.backgroundImage = "url('"+unsplashLink+"')"
 }
 
 
 // for(x of unsplashThis){
 // 	x.style.backgroundImage = "url('https://source.unsplash.com/random')";
 // }
+
+
+
+
+//Eduardo
+
+// function splashThis(){
+
+//     var array = {
+//       nature: "/category/nature",
+//       random: "/random"
+//     }
+
+//     let splashthis = document.querySelectorAll(".splash-this");
+//     let splashnature = document.querySelectorAll(".splash-this-nature");
+
+//     var splash = "url('https://source.unsplash.com:replace:')";
+
+//     for(let container of splashthis){
+//       container.style.background = splash.replace(":replace:",array.random);
+//     }
+
+//     for(let nature of splashnature){
+//       nature.style.background = splash.replace(":replace:",array.nature);
+//     }
+//   }
+
+//   window.onload = splashThis;
