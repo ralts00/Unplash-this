e45305aaf16cc3e7c6c411a63957960cdd4ca15d
Jonathan Coutiño <ralts00@MacBook-Pro-de-ComproPagoMB.local>
2016-03-04 18:30:53 -0600

    funciona :)

---
+++ unsplashThis.js
@@ -0,0 +1,24 @@
+// var unsplashThis = document.querySelectorAll(".unsplash-this")
+
+// console.log(unsplashThis);
+
+// for(var x = 0; x < unsplashThis.length; x++){
+// 	unsplashThis[x].style.backgroundImage = "url('https://source.unsplash.com/random')"
+// }
+
+
+
+
+//funciona
+
+var unsplashThis = document.querySelectorAll(".unsplash-this");
+// var unsplashLink = "https://source.unsplash.com/random"
+
+for(var x = 0; x < unsplashThis.length; x++){
+	unsplashThis[x].style.backgroundImage = "url('https://source.unsplash.com/random')"
+}
+
+
+// for(x of unsplashThis){
+// 	x.style.backgroundImage = "url('https://source.unsplash.com/random')";
+// }
