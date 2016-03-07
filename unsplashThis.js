4064a55a791a74e284e983ce4635257877233fd3
Jonathan Coutiño <ralts00@MacBook-Pro-de-Jonathan.local>
2016-03-07 01:52:40 -0600

    improvement obtaining different photos by category

--- unsplashThis.js
+++ unsplashThis.js
@@ -1,25 +1,14 @@
 var unsplashThis = document.querySelectorAll(".unsplash-this");
 
-var unsplashThisNew = document.querySelectorAll(".unsplash-this-new");
-
-var unsplashTnisNewLink = "https://source.unsplash.com/random";
-
-console.log(unsplashTnisNewLink)
-
-for(var x = 0; x < unsplashThisNew.length; x++){
- unsplashThisNew[x].setAttribute("style", "background-position: center; background-repeat: no-repeat; background-image: url('https://source.unsplash.com/random')");
-}
-
-
-
 function FunsplashThis(){
-    for(var x = 0; x < unsplashThis.length; x++){
+    for(var x = 0; x < unsplashThis.length; x++) {
         var unsplashCategory = unsplashThis[x].getAttribute("data-category");
-        var unsplashLink= "https://source.unsplash.com/random";
-        if (unsplashCategory){
-            unsplashLink = "https://source.unsplash.com/category/"+unsplashCategory;
+        var unsplashLink= "https://source.unsplash.com/random/" + x;
+        if (unsplashCategory) {
+            unsplashLink = "https://source.unsplash.com/category/"+unsplashCategory+"/?=" + x;
         }
         unsplashThis[x].setAttribute("style", "background-position: center; background-repeat: no-repeat; background-image: url('"+unsplashLink+"')");
+ 
     }
 }
 
